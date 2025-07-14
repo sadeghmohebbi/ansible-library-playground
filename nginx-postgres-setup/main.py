548e33c0c54b0109b82e292da397c1ae917b27fb
mohebbi <mohebbisadegh@gmail.com>
2025-07-15 00:48:36 +0330

    add install nginx task

--- nginx-postgres-setup/main.py
+++ nginx-postgres-setup/main.py
@@ -1,9 +1,17 @@
 import ansible_runner
 
-def install_nginx():
-    r = ansible_runner.run(host_pattern='localhost', module='apt', module_args='update-cache=True')
-    print("Status:", r.status)
-    print("Return Code:", r.rc)
-    return r
+def _tasks(*runs):
+    for r in runs:
+        print("Status:", r.status)
+        print("Return Code:", r.rc)
 
-install_nginx()
+def update_apt_cache():
+    return ansible_runner.run(host_pattern='localhost', module='apt', module_args='update-cache=True')
+
+def install_nginx():
+    return ansible_runner.run(host_pattern='localhost', module='apt', module_args='name=nginx state=present')
+
+_tasks(
+    update_apt_cache(),
+    install_nginx()
+)
